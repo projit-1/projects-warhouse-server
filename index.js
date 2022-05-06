d52f1cd2b2e17ba874dcd1d0bb0f4af6f1722c22
projit-1 <projitmondol@gmail.com>
2022-05-07 00:33:55 +0600

    mongodb and url is connected

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require("express");
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// midl - wire
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n7mbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('inventories').collection('inventory');
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
 app.get('/', (req, res) => {
-    res.send("node is working")
+    res.send("node is working perfectly and easily hahah nononono")
 })
 
 app.listen(port, () => {
-    console.log('express is running', port);
+    console.log('express is running gggg', port);
 })
